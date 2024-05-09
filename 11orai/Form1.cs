2d4206cc5d7cb3ccf4618395f14b5f2819401b4d
Cola39 <rezsekfz@gmail.com>
2024-05-09 10:24:19 +0200

    studies

---
+++ 11orai/Form1.cs
@@ -0,0 +1,32 @@
+namespace _11orai
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            panel1.Controls.Clear();
+
+            UserControl1 uc1 = new UserControl1();
+
+            panel1.Controls.Add(uc1);
+
+            uc1.Dock = DockStyle.Fill;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            panel1.Controls.Clear();
+
+            UserControl2 uc2 = new UserControl2();
+
+            panel1.Controls.Add(uc2);
+
+            uc2.Dock = DockStyle.Fill;
+        }
+    }
+}
